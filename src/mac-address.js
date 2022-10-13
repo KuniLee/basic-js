505c4822dc60e9a232a2df0f909a0988dc672125
Sitnikov <selnetys@gmail.com>
2022-10-13 21:18:48 +0500

    feat: complete mac address test task

--- src/mac-address.js
+++ src/mac-address.js
@@ -7,17 +7,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Your task is to check by given string inputString
  * whether it's a MAC-48 address or not.
  *
- * @param {Number} inputString
+ * @param {String} inputString mac address
  * @return {Boolean}
  *
  * @example
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function isMAC48Address(inputString) {
+return inputString.length === 17 && inputString.split("-").every(el=>/[a-f0-9]{2}/i.test(el))
 }
+
 module.exports = {
   isMAC48Address
 };
